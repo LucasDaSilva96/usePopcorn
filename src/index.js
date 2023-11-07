08d07704509caf5211e45ebb770187f8fd19a229
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-11-07 11:40:30 +0100

    Finished making the star-component reusable

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from './App';
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* <App />*/}
-    <StarRating maxRating={10}></StarRating>
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>{/* <App />*/}</React.StrictMode>);
